85446f1ff0918a6065727c750d32017c1d1123a0
Aashutosh Bajgain <abajgain@mail.sfsu.edu>
2020-09-24 14:10:33 -0700

    variables correctly mentioned

--- caesar.c
+++ caesar.c
@@ -9,30 +9,30 @@ int main(int argc, string argv[])
     if (argc == 2 && isdigit(*argv[1]))
     {
         //to convert the user input to an integer
-        int k = atoi(argv[1]);
+        int key = atoi(argv[1]);
 
         //ask for the user input
-        string s = get_string("plaintext: ");
+        string user_input = get_string("plaintext: ");
 
-        for (int i = 0, n = strlen(s) ; i < n; i++)
+        for (int index_count = 0, strlength = strlen(user_input) ; index_count < strlength; index_count++)
         {
             //check the index and see if it's lowercase alphabet betwwen a and z
-            if (s[i] >= 'a' && s[i] <= 'z')
+            if (user_input[index_count] >= 'a' && user_input[index_count] <= 'z')
             {
                 //used caesar's algorithm provided with sligh tweak on formula
-                printf("%c", (((s[i] - 'a') + k) % 26) + 'a');
+                printf("%c", (((user_input[index_count] - 'a') + key) % 26) + 'a');
             }
             //check the index and see if it's uppercase alphabet betwwen a and z
-            else if (s[i] >= 'A' && s[i] <= 'Z')
+            else if (user_input[index_count] >= 'A' && user_input[i] <= 'Z')
             {
                 //used caesar's algorithm provided with sligh tweak on formula
-                printf("%c", (((s[i] - 'A') + k) % 26) + 'A');
+                printf("%c", (((user_input[index_count] - 'A') + key) % 26) + 'A');
             }
 
             else
 
             {
-                printf("%c", s[i]);
+                printf("%c", user_input[index_count]);
             }
         }
 
