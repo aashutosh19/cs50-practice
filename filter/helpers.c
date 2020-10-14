fb994c344b0ba9966ad00d267718a59791b6003d
Aashutosh Bajgain <abajgain@mail.sfsu.edu>
2020-10-14 10:14:23 -0700

    Blurred Implemented

--- filter/helpers.c
+++ filter/helpers.c
@@ -21,7 +21,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     }
 }
 
-//stops max value at 255 preventing overflow
+// stops max value at 255 preventing overflow
 int limit(int RGB)
 {
     if (RGB > 255)
@@ -82,7 +82,106 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+//create a temporary image to be blurred
+    RGBTRIPLE temp[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            temp[i][j] = image[i][j];
+        }
+    }
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int sum_blue;
+            int sum_green;
+            int sum_red;
+            float counter;
+            sum_blue = sum_green = sum_red = counter = 0;
+
+            //corner pixel on bottom right
+            if (i >= 0 && j >= 0)
+            {
+                sum_red += temp[i][j].rgbtRed;
+                sum_green += temp[i][j].rgbtGreen;
+                sum_blue += temp[i][j].rgbtBlue;
+                counter++;
+            }
+            //corner pixel on bottom left
+            if (i >= 0 && j - 1 >= 0)
+            {
+                sum_red += temp[i][j-1].rgbtRed;
+                sum_green += temp[i][j-1].rgbtGreen;
+                sum_blue += temp[i][j-1].rgbtBlue;
+                counter++;
+            }
+            //corner pixel on top left
+            if (i - 1 >= 0 && j >= 0)
+            {
+                sum_red += temp[i-1][j].rgbtRed;
+                sum_green += temp[i-1][j].rgbtGreen;
+                sum_blue += temp[i-1][j].rgbtBlue;
+                counter++;
+            }
+            //corner pixel on top right
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                sum_red += temp[i-1][j-1].rgbtRed;
+                sum_green += temp[i-1][j-1].rgbtGreen;
+                sum_blue += temp[i-1][j-1].rgbtBlue;
+                counter++;
+            }
+
+            //pixels on bottom edge
+            if ((i >= 0 && j + 1 >= 0) && (i >= 0 && j + 1 < width))
+            {
+                sum_red += temp[i][j+1].rgbtRed;
+                sum_green += temp[i][j+1].rgbtGreen;
+                sum_blue += temp[i][j+1].rgbtBlue;
+                counter++;
+            }
+            //pixels on top edge
+            if ((i - 1 >= 0 && j + 1 >= 0) && (i - 1 >= 0 && j + 1 < width))
+            {
+                sum_red += temp[i-1][j+1].rgbtRed;
+                sum_green += temp[i-1][j+1].rgbtGreen;
+                sum_blue += temp[i-1][j+1].rgbtBlue;
+                counter++;
+            }
+            //pixels on left edge
+            if ((i + 1 >= 0 && j >= 0) && (i + 1 < height && j >= 0))
+            {
+                sum_red += temp[i+1][j].rgbtRed;
+                sum_green += temp[i+1][j].rgbtGreen;
+                sum_blue += temp[i+1][j].rgbtBlue;
+                counter++;
+            }
+            //pixels on right edge
+            if ((i + 1 >= 0 && j - 1 >= 0) && (i + 1 < height && j - 1 >= 0))
+            {
+                sum_red += temp[i+1][j-1].rgbtRed;
+                sum_green += temp[i+1][j-1].rgbtGreen;
+                sum_blue += temp[i+1][j-1].rgbtBlue;
+                counter++;
+            }
+
+            //middle pixels
+            if ((i + 1 >= 0 && j + 1 >= 0) && (i + 1 < height && j + 1 < width))
+            {
+                sum_red += temp[i+1][j+1].rgbtRed;
+                sum_green += temp[i+1][j+1].rgbtGreen;
+                sum_blue += temp[i+1][j+1].rgbtBlue;
+                counter++;
+            }
+            //find average colour value
+            image[i][j].rgbtRed = round(sum_red / counter);
+            image[i][j].rgbtGreen = round(sum_green / counter);
+            image[i][j].rgbtBlue = round(sum_blue / counter);
+        }
+    }
+return;
 }
 
 // Detect edges
